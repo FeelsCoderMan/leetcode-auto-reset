10601089650e2ec4a35a287d6246065d7d0b67da
Onur Aksoy <onurlul@hotmail.com>
2025-05-11 20:00:52 +0300

    feat: removed unnecessary service worker

--- src/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get('autoResetSetting', (result) => {
-        chrome.storage.local.set({ autoResetSetting: result.autoResetSetting || false });
-    });
-});
